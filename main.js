9c533bca4622984a379cef596b89ae832aec31d3
unnamed755 <xoliboyevdoniyor@gmail.com>
2024-10-19 16:05:44 +0500

    bot

---
+++ main.js
@@ -0,0 +1,5 @@
+var username = prompt ("ismingizni kiriting")
+var userage = prompt ("yoshingizni kiriting")
+var usertext = prompt("yashash joyingizni kiriting")
+var user = prompt ("o'qish joyingizni kiriting")
+console.log(username, userage, usertext, user);
